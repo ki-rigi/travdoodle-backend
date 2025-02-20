fe215c713009005aa3e77996007a8c7955f8fc36
kirigi <nkathafelicia@gmail.com>
2025-02-20 14:59:36 +0300

    switched database from sqlite to postgrate

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from datetime import datetime
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///travdoodle.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
